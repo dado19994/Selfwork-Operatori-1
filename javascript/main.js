2e9cef16ffd3695a1db4a18887bfc772dda26ece
Davide <dignazio90@gmail.com>
2024-08-13 17:56:12 +0200

    Selfwork Operatori 1

---
+++ javascript/main.js
@@ -0,0 +1,29 @@
+// let num1 = 17;
+// let num2 = 65;
+
+// console.log("il valore della variabile num1 e' " + num1);
+// console.log(`il valore della variabile num2 e' ${num2}`);
+
+// let stringa = 'Ciao a tutti';
+// stringa = 'Ciao mi chiamo Davide';
+// console.log(`il valore della variabile stringa e' ${stringa}`);
+
+
+// const PIGRECO = 3.14159265359;
+// PIGRECO = 3.14;
+// console.log(`il valore di PIGRECO e' ${PIGRECO}`);
+
+// SELFWORK OPERATORI 1
+
+let a = 34;
+let b = 78;
+let c = 25;
+let d = 99;
+let e = 176;
+
+let somma = a + b + c + d + e;
+let media = a + b + c + d + e / 5;
+console.log(`la somma tra i numeri equivale a ${somma} e la media equivale a ${media}`);
+
+
+
